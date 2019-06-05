81874dd736753dbc9f245a12690dec892ce412dc
Sergey Ostroukhov <s.a.ostroukhov@gmail.com>
2019-06-05 12:17:22 +0300

    Change print text

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     std::cout << "Hello Git" << std::endl;
     std::cout << "Hello Sergey!" << std::endl;
     for (int i = 0; i < 10; ++i) {
-	std::cout << "i = " << i << std::endl;
+	std::cout << "j = " << i << std::endl;
     }
     return 0;
 }
