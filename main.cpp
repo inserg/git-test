261733047e921a4057ad6a103852c1ec4b84e945
Sergey Ostroukhov <s.a.ostroukhov@gmail.com>
2019-06-05 11:09:21 +0300

    Revert "Add print number" bad code
    This reverts commit a0151c3bf2fe70cc2aff9a3412d45395f07b3437.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,5 @@ int main()
 {
     std::cout << "Hello Git" << std::endl;
     std::cout << "Hello Sergey!" << std::endl;
-    for (int i = 0; i < 10; ++i) {
-	std::cout << i << std::endl;
-    }
     return 0;
 }
